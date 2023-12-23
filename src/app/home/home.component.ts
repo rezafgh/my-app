a77e839dde57159b342d0127175557fb5f8baf81
Reza Faghani <reza.faghani@3shape.com>
2023-12-23 18:55:08 +0100

    adding housing location component adding housing location service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, inject} from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+import { TestBed } from '@angular/core/testing';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
   template: `
-    <section>
+  <section>
     <form>
       <input type="text" placeholder="Filter by city">
       <button class="primary" type="button">Search</button>
     </form>
   </section>
+  <section class="results">
+  <app-housing-location
+  *ngFor="let housingLocation of housingLocationList"
+  [housingLocation]="housingLocation">
+</app-housing-location>
+  </section>
   `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  constructor(){
+    this.housingLocationList= this.housingService.getAllHousingLocations();
+  }
+ 
 }
